0ae4e271cf55c8f7fad2c597c19d22e2d037dc23
ltrajkovic92 <lazar.trajkovic92@gmail.com>
2023-10-19 01:04:54 +0200

    Add Basic Test

---
+++ src/test/java/tests/BasicTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import helper.Helper;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public abstract class BasicTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+
+    @BeforeClass
+    public void setup () {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void beforeMethode () {
+        driver.navigate().to(baseUrl);
+    }
+
+    @AfterMethod
+    public void afterMethod (ITestResult testResult) throws IOException {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            Helper.takeScreenshotWithDateAndTime(driver, testResult.getName());
+        }
+    }
+
+    @AfterClass
+    public void afterClass () {
+        driver.quit();
+    }
+}
