dfbcdde56666bee525535b762e78e42177e57163
ltrajkovic92 <lazar.trajkovic92@gmail.com>
2023-10-19 23:39:05 +0200

    Add test: forbids visits to home url if not authenticated

---
+++ src/test/java/tests/AuthRoutesTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import retry.RetryAnalyzer;
+
+public class AuthRoutesTests extends BasicTest{
+
+    @Test (priority = 1, retryAnalyzer = RetryAnalyzer.class)
+    public void forbidsVisitsToHomeUrlIfNotAuthenticated () {
+        driver.navigate().to(baseUrl + pageUrl.homePage);
+
+        Assert.assertTrue(pageUrl.isLoginPage(),
+                "Should be redirected to the Login page.");
+    }
+}
