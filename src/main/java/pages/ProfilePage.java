5b984a62c31d601452d6bc7d32ee42e8eb45beed
ltrajkovic92 <lazar.trajkovic92@gmail.com>
2023-10-20 16:37:00 +0200

    Add test: checks input types

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,6 +17,49 @@ public class ProfilePage extends BasicPage {
     }
     public String getValueFromEmailInput () {
         return getEmailInput().getAttribute("value");
+    }
+    public String getTypeFromEmailInput () {
+        return getEmailInput().getAttribute("type");
+    }
+    public String getAttributeDisabledFromInputEmail () {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js.executeScript("return arguments[0].getAttribute('disabled');", getEmailInput()).toString();
+    }
+    public WebElement getNameInput () {
+        return driver.findElement(By.id("name"));
+    }
+    public String getTypeFromNameInput () {
+        return getNameInput().getAttribute("type");
+    }
+    public WebElement getCityInput () {
+        return driver.findElement(By.id("city"));
+    }
+    public String getTypeFromCityInput () {
+        return getCityInput().getAttribute("type");
+    }
+    public WebElement getCountryInput () {
+        return driver.findElement(By.id("country"));
+    }
+    public String getTypeFromCountryInput () {
+        return getCountryInput().getAttribute("type");
+    }
+    public WebElement getTwitterInput () {
+        return driver.findElement(By.id("urlTwitter"));
+    }
+    public String getTypeFromTwitterInput () {
+        return getTwitterInput().getAttribute("type");
+    }
+    public WebElement getGitHubInput () {
+        return driver.findElement(By.id("urlGitHub"));
+    }
+    public String getTypeFromGitHubInput () {
+        return getGitHubInput().getAttribute("type");
+    }
+    public WebElement getPhoneInput () {
+        return driver.findElement(By.id("phone"));
+    }
+    public String getTypeFromPhoneInput () {
+        return getPhoneInput().getAttribute("type");
     }
     public void waitUntilLoadDialogIsInvisible () {
         wait
--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -25,4 +25,34 @@ public class ProfileTests extends BasicTest {
 
         navPage.clickOnLogoutButton();
     }
+
+    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void checksInputTypes () {
+        String email = "admin@admin.com";
+        String password = "12345";
+
+        navPage.clickOnLoginButton();
+        loginPage.login(email,password);
+        navPage.clickMyProfileButton();
+        profilePage.waitUntilLoadDialogIsInvisible();
+
+        Assert.assertEquals(profilePage.getTypeFromEmailInput(), "email",
+                "The email input type should be 'email'");
+        Assert.assertEquals(profilePage.getAttributeDisabledFromInputEmail(), "disabled",
+                "The email input should be 'disabled'");
+        Assert.assertEquals(profilePage.getTypeFromNameInput(), "text",
+                "The name input type should be 'text'");
+        Assert.assertEquals(profilePage.getTypeFromCityInput(), "text",
+                "The city input type should be 'text'");
+        Assert.assertEquals(profilePage.getTypeFromCountryInput(), "text",
+                "The country input type should be 'text'");
+        Assert.assertEquals(profilePage.getTypeFromTwitterInput(), "url",
+                "The twitter input type should be 'url'");
+        Assert.assertEquals(profilePage.getTypeFromGitHubInput(), "url",
+                "The github input type should be 'url'");
+        Assert.assertEquals(profilePage.getTypeFromPhoneInput(), "tel",
+                "The phone input type should be 'url'");
+
+        navPage.clickOnLogoutButton();
+    }
 }
