c1f5a3add64bc533c5e7b87a4e6769a3371d9231
ltrajkovic92 <lazar.trajkovic92@gmail.com>
2023-10-20 17:50:07 +0200

    Add test: verify profile editing

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -1,9 +1,6 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,11 +28,24 @@ public class ProfilePage extends BasicPage {
     public String getTypeFromNameInput () {
         return getNameInput().getAttribute("type");
     }
+    public String getValueFromNameInput () {
+        return getNameInput().getAttribute("value");
+    }
+    public void clearAndTypeName (String name) {
+        getNameInput().sendKeys(Keys.chord(Keys.CONTROL, "a"),name);
+    }
     public WebElement getCityInput () {
         return driver.findElement(By.id("city"));
     }
     public String getTypeFromCityInput () {
         return getCityInput().getAttribute("type");
+    }
+    public String getValueFromCityInput () {
+        return getCityInput().getAttribute("value");
+    }
+    public void clearAndTypeCity (String city) {
+        getCityInput().sendKeys(Keys.chord(Keys.CONTROL, "a"),city);
+        getCityInput().sendKeys(Keys.ENTER);
     }
     public WebElement getCountryInput () {
         return driver.findElement(By.id("country"));
@@ -43,11 +53,23 @@ public class ProfilePage extends BasicPage {
     public String getTypeFromCountryInput () {
         return getCountryInput().getAttribute("type");
     }
+    public String getValueFromCountryInput () {
+        return getCountryInput().getAttribute("value");
+    }
+    public void clearAndTypeCountry (String country) {
+        getCountryInput().sendKeys(Keys.chord(Keys.CONTROL, "a"),country);
+    }
     public WebElement getTwitterInput () {
         return driver.findElement(By.id("urlTwitter"));
     }
     public String getTypeFromTwitterInput () {
         return getTwitterInput().getAttribute("type");
+    }
+    public String getValueFromTwitterInput () {
+        return getTwitterInput().getAttribute("value");
+    }
+    public void clearAndTypeTwitter (String twitter) {
+        getTwitterInput().sendKeys(Keys.chord(Keys.CONTROL, "a"),twitter);
     }
     public WebElement getGitHubInput () {
         return driver.findElement(By.id("urlGitHub"));
@@ -55,11 +77,29 @@ public class ProfilePage extends BasicPage {
     public String getTypeFromGitHubInput () {
         return getGitHubInput().getAttribute("type");
     }
+    public String getValueFromGitHubInput () {
+        return getGitHubInput().getAttribute("value");
+    }
+    public void clearAndTypeGitHub (String github) {
+        getGitHubInput().sendKeys(Keys.chord(Keys.CONTROL, "a"),github);
+    }
     public WebElement getPhoneInput () {
         return driver.findElement(By.id("phone"));
     }
     public String getTypeFromPhoneInput () {
         return getPhoneInput().getAttribute("type");
+    }
+    public String getValueFromPhoneInput () {
+        return getPhoneInput().getAttribute("value");
+    }
+    public void clearAndTypePhone (String phone) {
+        getPhoneInput().sendKeys(Keys.chord(Keys.CONTROL, "a"),phone);
+    }
+    public WebElement getSaveButton () {
+        return driver.findElement(By.className("btnSave"));
+    }
+    public void clickOnSaveButton () {
+        getSaveButton().click();
     }
     public void waitUntilLoadDialogIsInvisible () {
         wait
--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -55,4 +55,51 @@ public class ProfileTests extends BasicTest {
 
         navPage.clickOnLogoutButton();
     }
+
+    @Test (priority = 3)
+    public void editsProfile () {
+        String email = "admin@admin.com";
+        String password = "12345";
+        String name = "Lazar Trajkovic";
+        String phone = "+38161283223";
+        String city = "Bucaramanga";
+        String country = "Spain";
+        String twitter = "https://twitter.com/profile/lazar1232";
+        String github = "https://github.com/ltrajkovic92";
+
+        navPage.clickOnLoginButton();
+        loginPage.login(email,password);
+        navPage.clickMyProfileButton();
+        profilePage.waitUntilLoadDialogIsInvisible();
+
+        profilePage.clearAndTypeName(name);
+        profilePage.clearAndTypePhone(phone);
+        profilePage.clearAndTypeCity(city);
+        profilePage.clearAndTypeCountry(country);
+        profilePage.clearAndTypeTwitter(twitter);
+        profilePage.clearAndTypeGitHub(github);
+        profilePage.clickOnSaveButton();
+
+        messagePopUpPage.waitForSuccessfullyPopUpMessage();
+
+        Assert.assertTrue(messagePopUpPage.getSuccessfullyPopUpMessageText().contains("Profile saved successfuly"),
+                "Success pop-up message should contain 'Profile saved successfuly'");
+
+        Assert.assertEquals(profilePage.getValueFromEmailInput(), email,
+                "The email input value should be " + email);
+        Assert.assertEquals(profilePage.getValueFromNameInput(), name,
+                "The name input value should be " + name);
+        Assert.assertEquals(profilePage.getValueFromPhoneInput(), phone,
+                "The phone input value should be " + phone);
+        Assert.assertEquals(profilePage.getValueFromCityInput(), city,
+                "The city input value should be " + city);
+        Assert.assertEquals(profilePage.getValueFromCountryInput(), country,
+                "The country input value should be " + country);
+        Assert.assertEquals(profilePage.getValueFromTwitterInput(), twitter,
+                "The twitter input value should be " + twitter);
+        Assert.assertEquals(profilePage.getValueFromGitHubInput(), github,
+                "The github input value should be " + github);
+
+        navPage.clickOnLogoutButton();
+    }
 }
